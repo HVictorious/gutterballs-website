ea817eed2650e891682b8f74cab52683ab8bfffa
Hunter Victorious <108153042+HVictorious@users.noreply.github.com>
2024-05-05 15:13:51 -0400

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,21 +62,21 @@
 					}
 
 				// Video check.
-					var video = $banner.data('video');
+					//var video = $banner.data('video');
 
-					if (video)
-						$window.on('load.banner', function() {
+					//if (video)
+						//$window.on('load.banner', function() {
 
 							// Disable banner load event (so it doesn't fire again).
-								$window.off('load.banner');
+								//$window.off('load.banner');
 
 							// Append video if supported.
-								if (!skel.vars.mobile
-								&&	!skel.breakpoint('large').active
-								&&	skel.vars.IEVersion > 9)
-									$banner.append('<video autoplay loop><source src="' + video + '.mp4" type="video/mp4" /><source src="' + video + '.webm" type="video/webm" /></video>');
+								//if (!skel.vars.mobile
+								//&&	!skel.breakpoint('large').active
+								//&&	skel.vars.IEVersion > 9)
+									//$banner.append('<video autoplay loop><source src="' + video + '.mp4" type="video/mp4" /><source src="' + video + '.webm" type="video/webm" /></video>');
 
-						});
+						//});
 
 				// More button.
 					$banner.find('.more')
